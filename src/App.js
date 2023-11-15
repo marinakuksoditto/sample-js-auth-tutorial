94fa13ffebeda7be47da73a66c9f3626887d5a9f
Marina <marina.kukso@ditto.live>
2023-11-14 21:11:28 -1000

    set up auth0 scaffolding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect  } from "react";
 import './App.css';
 import Ditto from "./ditto"
+import { useAuth0 } from "@auth0/auth0-react"
 
 let ditto
 function App() {
+  const { user, loginWithRedirect, getAccessTokenSilently, isAuthenticated, logout } = useAuth0()
   const [cars, setCars] = useState(0)
   const [error, setError] = useState('')
 
@@ -35,6 +37,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {!isAuthenticated && <button onClick={() => loginWithRedirect()}>Log In</button>}
+        {isAuthenticated &&
+          <div>
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        }
         <div>
           <h3>
           {cars} cars
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { init } from "@dittolive/ditto"
+import { Auth0Provider } from "@auth0/auth0-react"
 
 (async () => {
   await init()
   const root = createRoot(document.getElementById('root'));
   root.render(
-    <App />
+    <Auth0Provider
+      domain="YOUR_DOMAIN_HERE"
+      clientId="YOUR_CLIENT_ID_HERE"
+      redirectUri={window.location.origin}
+      audience="YOUR_AUDIENCE_HERE"
+      scope="read:current_user update:current_user_metadata"
+    >
+      <App />
+    </Auth0Provider>
   );
 })()
